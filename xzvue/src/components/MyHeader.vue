6c59b17fe430b8643913bfa415c149e99594eb35
binbin <chenbin@qq.com>
2020-09-23 20:21:55 +0800

    9/23

--- xzvue/src/components/MyHeader.vue
+++ xzvue/src/components/MyHeader.vue
@@ -1,15 +1,12 @@
 <template>
-    <div id="nav">
-      <router-link to="/">Home</router-link>我是全局的头部
-      <router-link to="/about">About</router-link>
-      
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
 <style>
 #nav {
   padding: 30px;
-  background-color: aqua;
-  color: chocolate;
 }
 
 #nav a {
--- xzvue/src/main.js
+++ xzvue/src/main.js
@@ -2,15 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import MyHeader from "./components/MyHeader"
-import axios from 'axios'
-// import Carousel from "./views/carousel"
-// Vue.component("my-header", MyHeader)
+//引入MyHeader.vue文件中的模块对象内容，保存在变量MyHeader中
+import MyHeader from "./components/MyHeader" //扩展名.vue或.js都可省略
+import axios from "axios"
 
-axios.defaults.baseURL="http://xzserver.applinzi.com";
+//让MyHeader变成全局组件:
+Vue.component("my-header",MyHeader)
+// axios.defaults.baseURL="http://xzserver.applinzi.com";
+axios.defaults.baseURL="http://localhost:5050";
+//如果在客户端使用http代理跨域，baseURL不要写真实的服务器地址，而应该写http代替提供的接口地址的别名，比如: /api
+// axios.defaults.baseURL="/api";
 Vue.prototype.axios=axios;
 
 Vue.config.productionTip = false
+
 new Vue({
   router,
   store,
