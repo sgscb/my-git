aea596fdfa0563ebb44c9b304b616bc34672484e
bing <bing@123.com>
2021-01-27 00:28:02 +0800

    feat:ts练习

---
+++ Ts/03ex.ts
@@ -0,0 +1,37 @@
+// private 私有的  protected 受保护的 子类可以访问
+class Animal {
+  readonly name: string; // 只读
+  constructor(name: string) {
+    this.name = name
+  }
+  run() {
+    return `${this.name} is running`
+  }
+}
+const snake = new Animal('lily')
+// snake.name="132"
+// 继承的特性 
+class Dog extends Animal {
+  bark() {
+    return `${this.name} is barking`
+  }
+}
+
+const xiaobao = new Dog('xiaobao')
+console.log(xiaobao.run())
+console.log(xiaobao.bark())
+
+// 这里我们重写构造函数，注意在子类的构造函数中，必须使用 super 调用父类的方法，要不就会报错。
+class Cat extends Animal {
+  static categories = ['mammal']
+  constructor(name: string) {
+    super(name)
+    console.log(this.name)
+  }
+  run() {
+    return 'Meow, ' + super.run()
+  }
+}
+const maomao = new Cat('maomao')
+console.log(maomao.run()) //  ?----
+console.log(Cat.categories)
